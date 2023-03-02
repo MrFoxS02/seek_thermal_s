c8e3699e802d745da1a8cda7fe06cf800bafa54d
Fox_S <87873730+MrFoxS02@users.noreply.github.com>
2023-03-02 14:24:33 +0300

    Add files via upload

---
+++ examples/seek_test.cpp
@@ -0,0 +1,40 @@
+/*
+ *  Test program SEEK Thermal Compact/CompactXR
+ *  Author: Maarten Vandersteegen
+ */
+#include <opencv2/highgui/highgui.hpp>
+#include "seek.h"
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    int k = 0;
+    k += 1;
+    LibSeek::SeekThermal seek(argc == 2 ? argv[1] : "");
+    cv::Mat frame, grey_frame;
+
+    if (!seek.open()) {
+        std::cout << "failed to open seek cam" << std::endl;
+        return -1;
+    }
+
+    while(1) {
+        if (!seek.read(frame)) {
+            std::cout << "no more LWIR img" << std::endl;
+            return -1;
+        }
+
+        cv::normalize(frame, grey_frame, 0, 65535, cv::NORM_MINMAX);
+        //cv::imshow("LWIR", grey_frame);
+        if k % 20 == 0
+        {
+        	cv::imwrite("/Users/nickporter/Desktop/Gray_Image.bmp", grey_frame);
+        }
+
+	
+        char c = cv::waitKey(10);
+        if (c == 's') {
+            cv::waitKey(0);
+        }
+    }
+}
