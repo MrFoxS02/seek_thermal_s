1ec5727019c42c1e18147a21a70b92e2b0ff9468
Fox_S <87873730+MrFoxS02@users.noreply.github.com>
2023-03-02 19:36:22 +0300

    Add files via upload

--- examples/seek_test.cpp
+++ examples/seek_test.cpp
@@ -8,7 +8,6 @@
 
 int main(int argc, char** argv)
 {
-    int k = 0;
     LibSeek::SeekThermal seek(argc == 2 ? argv[1] : "");
     cv::Mat frame, grey_frame;
 
@@ -22,15 +21,10 @@ int main(int argc, char** argv)
             std::cout << "no more LWIR img" << std::endl;
             return -1;
         }
-	k += 1;
+
         cv::normalize(frame, grey_frame, 0, 65535, cv::NORM_MINMAX);
-        //cv::imshow("LWIR", grey_frame);
-     	cv::cvtColor(gray_frame, color, cv::COLOR_GRAY2BGR)
-     	if (k % 10 == 0)
-     	{ 
-        	cv::imwrite("/home/youbot/catkin_ws/src/libseek-thermal-master/photos/Gray_Image.jpg", grey_frame);
-	}
-	
+        cv::imshow("LWIR", grey_frame);
+
         char c = cv::waitKey(10);
         if (c == 's') {
             cv::waitKey(0);
